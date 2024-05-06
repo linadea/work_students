db9c8bcf8424f2d973d8e6f7991471bbf12281be
linadea <amartsinkovska@gmail.com>
2024-05-06 14:27:48 +0300

    Added constants Colors

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFFACFF25);
+const Color secondaryColor1 = Color(0xFF5DBFDE);
+const Color secondaryColor2 = Color(0xFFDDEFF3);
+const Color accentColor = Color(0xFFFF2F01);
+const Color textColorDark = Color(0xFF19191B);
+const Color textColorLight = Color(0xFF4D4D4D);
+const Color greyColor = Color(0xFF757575);
+const Color backgroundColor = Color(0xFFDEDEDE);
+const Color containerColor = Color(0xFFFBFBFB);
